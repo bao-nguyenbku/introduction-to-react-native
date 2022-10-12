df35e649e088621eb13269a3b5fc2690d3f1c9f3
Bao Nguyen Thien <65020023+bao-nguyenbku@users.noreply.github.com>
2022-10-12 15:58:10 +0700

    implement blogs

--- screens/Blogs.js
+++ screens/Blogs.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
-
+import blogs from '../api/blogs';
+import BlogItem from '../components/BlogItem';
 const Blogs = () => {
   return (
-    <View>
-      <Text>Blogs</Text>
-    </View>
+    <ScrollView>
+      {blogs && blogs.map(blog => {
+        return (
+          <BlogItem 
+            key={blog.id}
+            blog={blog}
+          />
+        )
+      })}
+    </ScrollView>
   )
 }
 
