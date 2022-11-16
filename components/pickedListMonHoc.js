994baa94751f0a435b4e060fb998cc22dcdbadd0
Thanh Hieu <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-11-16 15:44:00 +0700

    add picked list

--- components/pickedListMonHoc.js
+++ components/pickedListMonHoc.js
@@ -1,8 +1,36 @@
 import { useState } from "react";
 import { ScrollView } from "react-native";
 
-const pickedListMonHoc = (props) => {
-    const [pickedList, setPickedList] = useState([]);
+const course = [
+    {
+      id: '238sdhfksdfs',
+      name: "App Mobile Dev"
+    },
+    {
+      id: 'dsjgdufger454',
+      name: "DSA"
+    },
+    {
+      id: '49584dfgkdfhg0803242',
+      name: "PPL"
+    },
+    {
+      id: 'fdjgd456402482sd',
+      name: "System Architecture"
+    },
+    {
+      id: '58dfjd42204282aada',
+      name: "Software Engineering"
+    },
+    {
+      id: '456840djgd08-23234256',
+      name: "Nhập môn Lập trình"
+    },
+  ]
+  
+
+const PickedListMonHoc = (props) => {
+    const [pickedList, setPickedList] = useState([...course]);
 
     return (<ScrollView>
 
@@ -10,8 +38,10 @@ const pickedListMonHoc = (props) => {
             return (<View key={index}>
                 <Text>{item.id}</Text>
             </View>)
-        })
+        })}
         
     </ScrollView>)
     
 }
+
+export default PickedListMonHoc;
--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-// import { increment, decrement } from '../store/reducer/blog';
+import React, { useEffect } from 'react'
 import { getAllCourses } from '../api/blogs';
+import PickedListMonHoc from '../components/pickedListMonHoc';
+
 const Home = () => {
-  const { count } = useSelector(state => state.blog);
-  const dispatch = useDispatch();
-  getAllCourses().then(result => console.log(result));
+  
+  useEffect(() =>{
+    getAllCourses().then(result => console.log(result));
+  }, []);
+
   return (
-    <Text>Hello world</Text>
-  )
+    <View style={styles.container}> 
+      <PickedListMonHoc />
+    </View>
+  );
 }
 
 export default Home
